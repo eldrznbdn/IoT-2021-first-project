dbba1d1ce3853a84420cfb219d0184895468cd8b
Name Surname <92988582+eldrznbdn@users.noreply.github.com>
2021-11-05 12:33:03 +0200

    add laba3 code

---
+++ laba3.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int numbers[8] = { 1,2,3,4,5,7,8,9 };
+	for (int i = 0; i < 8; i++)
+	{
+		cout << numbers[i] << " ";
+	}
+	for (int i = 1; i < 8; i++)
+	{
+		if (numbers[i - 1] + 1 != numbers[i])
+		{
+			cout << endl << numbers[i];
+			return numbers[i];
+		}
+	}
+	return 0;
+
+}
