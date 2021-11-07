e1173c268756256303e135b78df94304f025857b
eldrznbdn <92988582+eldrznbdn@users.noreply.github.com>
2021-11-07 22:05:00 +0200

    Update laba3.cpp

--- laba3.cpp
+++ laba3.cpp
@@ -1,20 +1,25 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
+
+void cout_array(int numbers[]) {
+    for (int i = 0; i < 8; i++)
+    {
+        cout << numbers[i] << " ";
+    }
+}
+int extra_number(int  numbers[]) {
+    for (int i = 1; i < 8; i++)
+    {
+        if (numbers[i - 1] + 1 != numbers[i])
+        {
+            return numbers[i];
+        }
+    }
+}
 int main()
 {
-	int numbers[8] = { 1,2,3,4,5,7,8,9 };
-	for (int i = 0; i < 8; i++)
-	{
-		cout << numbers[i] << " ";
-	}
-	for (int i = 1; i < 8; i++)
-	{
-		if (numbers[i - 1] + 1 != numbers[i])
-		{
-			cout << endl << numbers[i];
-			return numbers[i];
-		}
-	}
-	return 0;
-
+    int numbers[8] = { 1,2,3,4,5,7,8,9 };
+    cout_array(numbers);
+    cout << endl << extra_number(numbers);
+    return 0;
 }
